6ac2bcfd11923c24c13a218e2bd9840bc94605fa
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-10-25 20:36:41 +0200

    definizione costruttore Movie

--- index.php
+++ index.php
@@ -10,3 +10,14 @@ class Movie
     private $imgPath;
 
 
+    function __construct($_title, $_year, $_genre = '', $_rating = 0, $_imgPath = '')
+    {
+        $this->title = $_title;
+        $this->year = $_year;
+        $this->genre = $_genre;
+        $this->rating = $_rating;
+
+        $this->imgPath = $_imgPath;
+    }
+
+
